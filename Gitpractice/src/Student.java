12852fd0a75809d6cc35674d2c471ca2b46ea0fe
User <User@PANINI10>
2019-03-24 21:09:11 -0500

    1st commit

--- Gitpractice/src/Student.java
+++ Gitpractice/src/Student.java
@@ -4,16 +4,7 @@ public class Student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//changes from branch
-		//changes from git
-		//changes from branch_1
-
-		//changes from branch_2
-
-		//changes from branch_3
-
-    //changes from git
-	//change from git2	
-
+		
 	}
 
 }
